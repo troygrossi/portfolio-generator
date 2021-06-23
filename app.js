2f57a0a145cbe4151672f877affbd534781b14b9
Troy Grossi <ttroygrossit@gmail.com>
2021-06-23 16:56:28 -0700

    html template modularized and working

---
+++ app.js
@@ -0,0 +1,14 @@
+const fs = require("fs");
+
+const profileDataArgs = process.argv.slice(2);
+
+const [name, github] = profileDataArgs;
+
+const generatePage = require("./src/template.js");
+generatePage(name, github);
+
+fs.writeFile("./index.html", generatePage(name, github), (err) => {
+  if (err) throw new Error(err);
+
+  console.log("Portfolio complete! Check out index.html to see the output!");
+});
